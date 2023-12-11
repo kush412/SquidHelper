495a61dd96229b97d9b61d3e3d4517108c0aa4a7
An Dang <dangphucan2002@gmail.com>
2023-12-11 16:51:12 +0700

    fix chown

--- modules/utils.py
+++ modules/utils.py
@@ -260,13 +260,13 @@ def put_proxy_config(hostname, username, password, key_filename, passphrase, con
         except:
             pass
         sftp_client.put(config_file, f'{config.SQUID_PATH}/squid.conf')
-        command = 'sudo -S chmod -R 700 /etc/squid/ssl_cert && sudo -S chown -R proxy:proxy /etc/squid/ssl_cert'
+        command = 'sudo -S chmod -R 700 /etc/squid/ssl_cert'
         if username != 'root' and not password:
             prompt = getpass('[+] Enter password to backup Squid: ')
         else:
             prompt = password
         run_command(hostname, username, password, key_filename,
-                        passphrase, command, prompt=f"{prompt}\n{prompt}")
+                        passphrase, command, prompt=f"{prompt}")
         if working_dir_ip == '':
             sftp_client.put(config.SAMPLE_PEM, config.SQUID_PEM)
             sftp_client.put(config.SAMPLE_DER, config.SQUID_DER)
@@ -300,13 +300,13 @@ def put_proxy_config(hostname, username, password, key_filename, passphrase, con
 
     except Exception as e:
         print(f'An error occurred in putting new config: {e}')
-    command = f'sudo -S chmod -R 744 /etc/squid && sudo -S squid -f {config.SQUID_PATH}/squid.conf'
+    command = f'sudo -S chmod -R 744 /etc/squid && sudo -S chown -R proxy:proxy /etc/squid/ssl_cert && sudo -S squid -f {config.SQUID_PATH}/squid.conf'
     if not password and username != 'root':
         prompt = getpass('[+] Enter password to restart Squid: ')
     else:
         prompt = password
     run_command(hostname, username, password, key_filename,
-                passphrase, command, prompt=f"{prompt}\n{prompt}")
+                passphrase, command, prompt=f"{prompt}\n{prompt}\n{prompt}")
 
     print('[*] Restarting squid service')
     command = 'sudo -S systemctl restart squid'
